aaaf12278bdef1a26b08a8d2f5e518d6fb055cab
Bilal Ahmad <bilalahmad2003170@gmail.com>
2024-09-11 12:26:02 +0500

    crud

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose= require("mongoose")
+const dotenv=require("dotenv")
+const bodyParser = require('body-parser')
+
+
+
+const app = express()
+app.use(bodyParser.json())
+const PORT = process.env.PORT || 3000;
+const MONGOURL=process.env.MONGO_URL;
+
+
+mongoose.connect(MONGOURL).then(()=>{
+  console.log("database connected")
+  app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`)
+})
+
+}).catch((err)=>{
+  console.log(err)
+})
+
+
+
